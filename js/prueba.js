bba65b7f5a8142caa3c4632690e826486a32fa51
alejandro <limonmagico1@gmail.com>
2024-03-23 14:00:14 +0100

    Prueba1

--- js/prueba.js
+++ js/prueba.js
@@ -1,5 +1,5 @@
 async function traerPagina(){
-    const pagina = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const pagina = await fetch('https://yelmocines.es/cartelera/la-rioja/premium-berceo');
     const datos = await pagina.json();
     console.log(datos);
 }
